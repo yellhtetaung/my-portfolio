2899e81873696b34b0caf049cfa8810437670074
yellhtetaung <yehtetaung.dev@gmail.com>
2024-02-22 17:06:35 +0630

    :wrench: configuration(yehtetaung): husky and git commit message

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    extends: ["gitmoji"],
+    rules: {
+        "header-max-length": [0, "always", 100],
+        "scope-case": [0, "always", "pascal-case"],
+        "type-enum": [
+            2,
+            "always",
+            [
+                "feat",
+                "fix",
+                "style",
+                "package",
+                "configuration",
+                "remove",
+                "revert",
+                "build",
+                "pull",
+                "dependency",
+            ],
+        ],
+    },
+};
